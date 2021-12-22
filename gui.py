296a1504e74132c436a45e331c63fc0621e78980
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-12-21 22:20:47 -0500

    Add main files

---
+++ gui.py
@@ -0,0 +1,187 @@
+from PySide6 import QtCore, QtGui, QtWidgets as q
+from PIL.ImageQt import ImageQt
+from PIL import Image
+from PIL.ImageOps import grayscale
+
+class mainWin( q.QWidget ):
+	def __init__( self ):
+		super().__init__()
+
+		self.setWindowTitle( 'PBR2Source' )
+		self.resize( 600, 350 )
+
+		''' --------------- LAYOUT --------------- '''
+
+		self.layout = q.QVBoxLayout( self )
+
+		self.innerLayout = q.QHBoxLayout()
+		self.layout.addLayout( self.innerLayout )
+
+		self.footer = q.QHBoxLayout()
+		self.layout.addLayout( self.footer )
+
+		''' --------------- INPUT  --------------- '''
+
+		self.boxIn = q.QGroupBox( 'PBR Input' )
+		self.innerLayout.addWidget( self.boxIn )
+
+		self.inputLayout = q.QGridLayout( self.boxIn )
+		self.inputLayout.setHorizontalSpacing( 1 )
+		self.inputLayout.setAlignment( QtCore.Qt.AlignTop )
+
+		def addInputForm( name, row, caption, BW ):
+			label = q.QLabel()
+			label.setFixedSize( 48, 48 )
+			label.setBackgroundRole( QtGui.QPalette.ColorRole.Mid )
+			label.setAutoFillBackground( True )
+
+			self.inputLayout.addWidget( label, row, 0, 2, 1 )
+			self.inputLayout.addWidget( q.QLabel(name), row, 1, 1, 2, alignment=QtCore.Qt.AlignRight )
+			btn = q.QPushButton( 'Browse' )
+			btn.setFixedWidth( 50 )
+			self.inputLayout.addWidget( btn, row+1, 1, alignment=QtCore.Qt.AlignRight )
+			lineedit = q.QLineEdit()
+			lineedit.setDisabled( True )
+			lineedit.setAlignment( QtCore.Qt.AlignRight )
+			self.inputLayout.addWidget( lineedit, row+1, 2 )
+
+			def handleBrowse():
+				filepath = q.QFileDialog.getOpenFileName( caption=f'Select {caption} image', filter='Images (*.png *.jpg *.jpeg *.bmp *.tga)' )
+
+				if filepath[0] != '':
+					# Load image with PIL, then convert it to something QT can use.
+					with Image.open( filepath[0] ) as img:
+						if BW: img = grayscale(img)
+						self.onImageLoaded( caption.capitalize(), img )
+						qimg = ImageQt( img )
+
+					pixmap = QtGui.QPixmap.fromImage( qimg )
+					scaled = pixmap.scaled( QtCore.QSize(48, 48) )
+					label.setPixmap( scaled )
+
+					lineedit.setText( filepath[0] )
+
+			btn.clicked.connect( handleBrowse )
+
+		addInputForm( 'Diffuse', 0, 'diffuse', False )
+		addInputForm( 'AO (Optional)', 2, 'ao', True )
+		addInputForm( 'Roughness', 4, 'roughness', True )
+		#addInputForm( 'Metallic', 6, 'metallic', True )
+		addInputForm( 'Normal Map', 8, 'normal', False )
+
+		''' --------------- OUTPUT --------------- '''
+
+		self.boxOut = q.QGroupBox( 'Source Output' )
+		self.innerLayout.addWidget( self.boxOut )
+
+		self.outputLayout = q.QVBoxLayout( self.boxOut )
+		self.outputLayout.setAlignment( QtCore.Qt.AlignTop )
+		self.outputLayout.setSpacing( 1 )
+
+
+		self.outputLayout.addWidget( q.QLabel('Shader') )
+		self.shaderInput = q.QComboBox()
+		self.shaderInput.addItems([
+			'LightmappedGeneric',
+			'VertexLitGeneric'
+		])
+		self.shaderInput.setEditable( True )
+		self.shaderInput.setCurrentText( 'LightmappedGeneric' )
+		self.shaderInput.setInsertPolicy( q.QComboBox.NoInsert )
+		self.shaderInput.currentTextChanged.connect( lambda x: self.setProperty( 'OutputShader', x ) )
+		self.outputLayout.addWidget( self.shaderInput )
+
+		self.outputLayout.addSpacing( 10 )
+
+		self.outputLayout.addWidget( q.QLabel('Reflections') )
+		self.cubemapInput = q.QComboBox()
+		for a,b in [
+			('None', None),
+			('Cubemap', 'env_cubemap'),
+			('(P2) Black Wall 002a', 'metal/black_wall_envmap_002a'),
+			('(CSGO) Generic Metal 01', 'environment maps/metal_generic_001'),
+			('(CSGO) Generic Metal 02', 'environment maps/metal_generic_002'),
+			('(CSGO) Generic Metal 03', 'environment maps/metal_generic_003'),
+			('(CSGO) Generic Metal 04', 'environment maps/metal_generic_004'),
+			('(CSGO) Generic Metal 05', 'environment maps/metal_generic_005'),
+			('(CSGO) Generic Metal 06', 'environment maps/metal_generic_006')
+		]: self.cubemapInput.addItem( a, b )
+
+		self.cubemapInput.setEditable( True )
+		self.cubemapInput.setInsertPolicy( q.QComboBox.NoInsert )
+		self.cubemapInput.setCurrentIndex( 0 )
+		self.outputLayout.addWidget( self.cubemapInput )
+
+		# Handles when to take custom text and when to use the full path of the presets
+		def evalCustomInput( x ):
+			if (ind := self.cubemapInput.findText( x )) != -1:
+				self.setProperty( 'OutputEnvmap', self.cubemapInput.itemData(ind) )
+			else:
+				self.setProperty( 'OutputEnvmap', x )
+		self.cubemapInput.currentTextChanged.connect( evalCustomInput )
+
+		self.outputLayout.addSpacing( 10 )
+		
+		reflectIntensityLabel = q.QLabel('Reflection Intensity (30%)')
+		self.outputLayout.addWidget( reflectIntensityLabel )
+
+		self.reflectIntensityInput = q.QSlider( QtCore.Qt.Horizontal )
+		self.reflectIntensityInput.setRange( 0, 100 )
+		self.reflectIntensityInput.setSingleStep( 5 )
+		self.reflectIntensityInput.setValue( 30 )
+
+		def onSliderUpdate(x):
+			reflectIntensityLabel.setText(f'Reflection Intensity ({x}%)')
+			self.setProperty( 'OutputReflectIntensity', x )
+
+		self.reflectIntensityInput.valueChanged.connect( onSliderUpdate )
+		self.outputLayout.addWidget( self.reflectIntensityInput )
+
+		self.outputLayout.addStretch()
+
+		self.imageOptionsGroup = q.QGroupBox( 'Image Options' )
+		self.outputLayout.addWidget( self.imageOptionsGroup )
+		self.imageOptionsGroup.layout = q.QVBoxLayout( self.imageOptionsGroup )
+
+		self.checkFlipY = q.QCheckBox('Flip Normal Y')
+		self.checkFlipY.setChecked( True )
+		self.imageOptionsGroup.layout.addWidget( self.checkFlipY )
+
+
+		self.resizeToInputLayout = q.QHBoxLayout()
+		self.resizeToInputLayout.setAlignment( QtCore.Qt.AlignLeft )
+		self.imageOptionsGroup.layout.addLayout( self.resizeToInputLayout )
+
+		self.resizeToInputLayout.addWidget( q.QLabel('Resize:') )
+
+		def handleResizeInputChange(x):
+			if x == 0: self.setProperty( 'SizeOverride', False )
+			else:      self.setProperty( 'SizeOverride', 32 * 2**x )
+
+		self.resizeToInput = q.QComboBox()
+		self.resizeToInput.addItems(['Don\'t Resize','64x','128x','256x','512x','1024x','2048x','4096x'])
+		self.resizeToInputLayout.addWidget( self.resizeToInput )
+		self.resizeToInput.currentIndexChanged.connect( handleResizeInputChange )
+
+		''' --------------- FOOTER --------------- '''
+
+		self.footer.setSpacing( 0 )
+
+		self.progressBar = q.QProgressBar()
+		self.progressBar.setRange( 0, 5 )
+		self.progressBar.setFormat('%v/%m')
+		self.footer.addWidget( self.progressBar )
+
+		self.exportBtn = q.QPushButton( 'Export All' )
+		self.footer.addWidget( self.exportBtn )
+		self.exportBtn.clicked.connect( self.doExport )
+
+	def setProperty( self, prop, val ):
+		print(f'DEBUG: {prop} updated to value {val}')
+		pass
+
+	def doExport( self, path ):
+		pass
+
+	def onImageLoaded( self, name, img ):
+		self.setProperty( 'Image'+name, img )
---
+++ main.py
@@ -0,0 +1,203 @@
+from PIL.ImageOps import flip
+from PySide6.QtWidgets import QApplication, QMessageBox, QErrorMessage, QFileDialog
+from gui import mainWin
+from pathlib import Path
+from math import log2
+
+from os.path import join as joinPath
+from tempfile import gettempdir
+
+from PIL import Image, ImageChops
+from srctools import VTF
+from srctools.vtf import ImageFormats as VTFFormats
+
+class frontend( mainWin ):
+	def __init__( self ):
+		super().__init__()
+
+		self.data = {
+			'OutputShader': 'LightmappedGeneric',
+			'OutputEnvmap': 'None',
+			'OutputReflectIntensity': 30,
+			'ImageDiffuse': None,
+			'ImageRoughness': None,
+			#'ImageMetallic': None,
+			'ImageNormal': None,
+			'SizeOverride': False
+		}
+
+		self.exportBtn.setDisabled( True )
+
+	def setProperty( self, property, value ):
+		self.data[property] = value
+
+		self.exportBtn.setEnabled( all([
+			self.data['ImageDiffuse'],
+			self.data['ImageRoughness'],
+			#self.data['ImageMetallic'],
+			self.data['ImageNormal']
+		]) )
+
+	def getPathRelative( self, origpath:Path ):
+		newpath = origpath
+		while newpath.name != 'materials' and newpath != newpath.parent:
+			newpath = newpath.parent
+		
+		if newpath == newpath.parent: return None
+		return origpath.relative_to( newpath )
+	
+	def removeSuffix( self, path:Path ):
+		if path is None: return None
+		return Path( str(path)[:-len(path.suffix)] )
+
+	def doExport( self ):
+
+		''' ---------- CHECK IMAGES ---------- '''
+
+		def checkImageSize( img ):
+			def isPowerOfTwo( v ): return log2(v)%1 == 0
+			return isPowerOfTwo(img.width) and isPowerOfTwo(img.height)
+
+		for img in [
+			self.data['ImageDiffuse'],
+			#self.data['ImageMetallic'], # Roughness is baked into the ImageDiffuse texture, so it'll be resized anyways.
+			self.data['ImageNormal']]:
+			if img is None: continue
+			if not checkImageSize( img ):
+				msg = QErrorMessage( self )
+				msg.showMessage( 'An image has a non-power of two size!' )
+				msg.activateWindow()
+				return
+
+
+		''' ---------- DO PATH MAGIC ---------- '''
+
+		rawpath = QFileDialog.getSaveFileName( caption='Create material', filter='Valve Material Type (*.vmt)' )
+		if not len(rawpath[0]): return
+		path = rawpath[0]
+
+		vmtPathRelative = self.removeSuffix(self.getPathRelative( Path(path) ))
+
+		if vmtPathRelative is None:
+			msg = QErrorMessage( self )
+			msg.setWindowTitle( 'Error' )
+			msg.showMessage( 'Failed to find materials directory! Be sure that you are exporting into a content folder.' )
+			msg.activateWindow()
+			return
+
+		vmtPathStr = str( vmtPathRelative ).replace( '\\', '/' )
+		vmtDirRelative = vmtPathRelative.parent
+
+
+		''' ---------- WRITE VMT ---------- '''
+
+		self.progressBar.setValue( 1 )
+
+		with open( path, 'w' ) as vmt:
+			if self.data['OutputEnvmap'] == None:
+				vmt.writelines([
+					'"'+self.data.get('OutputShader')+'"\n',
+					'{\n',
+					'    $basetexture          "'+vmtPathStr+'_basecolor"\n',
+					'    $bumpmap              "'+vmtPathStr+'_bump"\n',
+					'}'
+				])
+			else:
+				vmt.writelines([
+					'"'+self.data.get('OutputShader')+'"\n',
+					'{\n',
+					'    $basetexture          "'+vmtPathStr+'_basecolor"\n',
+					'    $bumpmap              "'+vmtPathStr+'_bump"\n',
+					'\n',
+					'    $envmap               "'+self.data['OutputEnvmap']+'"\n',
+					'    $envmaptint           '+str(round(self.data['OutputReflectIntensity']/100,2))+'\n',
+					'    $envmaplightscale     0.98\n',
+					'    $basealphaenvmapmask  1\n',
+					'}'
+				])
+
+		''' ---------- PROCESS BASETEXTURE ---------- '''
+
+		self.progressBar.setValue( 2 )
+
+		def applyAlphaToTexture( tex:Image, alpha:Image ):
+			r, g, b = tex.split()
+			r2, g2, b2 = alpha.split()
+			return Image.merge( 'RGBA', (r, g, b, r2) )
+
+		# Register basetexture
+
+		BaseTexture = self.data['ImageDiffuse'].convert('RGB')
+		if self.data['SizeOverride']: BaseTexture = BaseTexture.resize( (self.data['SizeOverride'], self.data['SizeOverride']) )
+
+		# Register AO, Apply AO to basetexture
+
+		AOTexture = None
+		if self.data.get('ImageAo') != None:
+			AOTexture = self.data['ImageAo'].convert('RGB').resize( (BaseTexture.width,BaseTexture.height) )
+			BaseTexture = ImageChops.multiply( BaseTexture, AOTexture )
+
+		# Register roughness, apply roughness to basetexture (ONLY IF ENVMAPS ARE ENABLED)
+
+		if self.data['OutputEnvmap'] != None:
+			RoughnessTexture = self.data['ImageRoughness'].convert('RGB').resize( (BaseTexture.width,BaseTexture.height) )
+			BaseTexture = applyAlphaToTexture( BaseTexture, RoughnessTexture )
+
+
+		''' ---------- PROCESS NORMAL MAP ---------- '''
+
+		self.progressBar.setValue( 3 )
+
+		NormalTexture = self.data['ImageNormal'].convert('RGB')
+		if self.data['SizeOverride']: NormalTexture = NormalTexture.resize( (self.data['SizeOverride'], self.data['SizeOverride']) )
+
+		if self.checkFlipY.isChecked():
+			def flipG( tex ):
+				r, g, b = tex.split()
+				g = ImageChops.invert(g)
+				return Image.merge( 'RGB', (r, g, b) )
+			NormalTexture = flipG( NormalTexture )
+
+
+		''' ---------- CONVERT IMAGES ---------- '''
+		
+		self.progressBar.setValue( 4 )
+
+		def PILToVTF( img:Image, fmt ) -> VTF:
+			v = VTF( img.width, img.height, frames=1, fmt=fmt )
+			v.get( frame=0 ).copy_from( img.convert('RGBA').tobytes() )
+			return v
+
+		if self.data['OutputEnvmap'] == None:
+			BaseTextureVTF = PILToVTF( BaseTexture, VTFFormats.RGB888 )
+		else:
+			BaseTextureVTF = PILToVTF( BaseTexture, VTFFormats.RGBA8888 )
+		NormalTextureVTF = PILToVTF( NormalTexture, VTFFormats.UV88 )
+
+		targetDir = str( self.removeSuffix(Path(path)) )
+
+
+		''' ---------- SAVE IMAGES ---------- '''
+
+		with open( targetDir+'_basecolor.vtf', 'wb' ) as targetfile:
+			BaseTextureVTF.save( targetfile )
+		with open( targetDir+'_bump.vtf', 'wb' ) as targetfile:
+			NormalTextureVTF.save( targetfile )
+
+		self.progressBar.setValue( 5 )
+
+		msgBox = QMessageBox()
+		msgBox.setWindowTitle( self.windowTitle() )
+		msgBox.setText( 'Export successful!' )
+		msgBox.exec()
+
+		self.progressBar.reset()
+
+		
+		
+
+
+app = QApplication()
+win = frontend()
+win.show()
+app.exec()
